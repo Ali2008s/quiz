3b68c99dd8e0b941242209b1e37af003ef30a309
Ali <jhgddd04@gmail.com>
2026-03-22 18:44:31 +0300

    Update app icons and flutter launcher icons configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'screens/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MakhmakhaApp());
 }
 
